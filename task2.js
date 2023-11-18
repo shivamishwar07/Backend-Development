204281d3ceaccb739c269359b20cd00c066864d4
shivamishwar07 <shivam.kumar77087@gmail.com>
2023-11-18 13:02:12 +0530

    task2 is completed

---
+++ task2.js
@@ -0,0 +1,13 @@
+let product=((num1,num2)=>{
+    return num1*num2;
+})
+console.log(product(4,4));
+
+const name="shivam";
+const age="21";
+const hobbies="Cricles";
+
+const myData=((name,age,hobbies)=>{
+    return `My name is ${name } and my age is ${age} and my hobbie is ${hobbies}`;
+})
+console.log(myData(name,age,hobbies));
